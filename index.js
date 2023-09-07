f3a0b0cf8e1926c54e0fcb46f98d737c0783f11b
chrisbrody <chrisbrody@codifyacademy.com>
2023-09-07 13:10:42 -0400

    feat: scratchy audio is streamed back to user with twilio & elevenlabs, still need to encode audio properly

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const WebSocket = require("ws");
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const axios = require('axios');
 const WaveFile = require("wavefile").WaveFile;
+const fs = require('fs');
+const path = require("path");
+const twilio = require('twilio');
 
-const path = require("path")
+require('dotenv').config();
+
 const app = express();
-const server = require("http").createServer(app);
+const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-const axios = require('axios')
 
 let assembly;
 let chunks = [];
@@ -14,75 +19,83 @@ let transcribedText = "";
 let streamId = "";
 let currentCallSid = "";
 let ws = "";
-const websocketStreamUrl = 'wss://https://3cef-67-241-88-254.ngrok-free.app/audio';
 
-require('dotenv').config(); // Load environment variables from .env file
-
-const ASSEMBLY_APIKEY = process.env.ASSEMBLY_APIKEY
-
-// add twilio
-const twilio = require('twilio');
+const ASSEMBLY_APIKEY = process.env.ASSEMBLY_APIKEY;
+const assemblyVoiceId = "21m00Tcm4TlvDq8ikWAM";
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const XI_API_KEY = process.env.ELEVENLABS_APIKEY;
+
+const TWILIO_AUDIO_CHUNK_THRESHOLD = 5;
+
+const MODEL = 'gpt-3.5-turbo';
+const MAXTOKENS = 4000;
+const TEMPERATURE = 0.5;
+
+const endpoint = 'https://api.openai.com/v1/chat/completions';
+
+const chatGptHeaders = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${process.env.OPENAI_APIKEY}`
+};
+
+const ttsHeaders = {
+  'Accept': 'audio/mpeg',
+  'Content-Type': 'application/json',
+  'xi-api-key': XI_API_KEY
+};
+
+const ttsUrl = `https://api.elevenlabs.io/v1/text-to-speech/${assemblyVoiceId}/stream`;
+
+const appPort = 8080;
+
+// Initialize Twilio client
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-const fs = require('fs')
 
-// send question to Large language model
-async function sendToChatGPT (transcribedText) {
+// Send question to Large language model
+async function sendToChatGPT(transcribedText) {
   console.log(`Transcribed Text: ${transcribedText}`);
 
-  const MODEL = 'gpt-3.5-turbo'
-  const MAXTOKENS = 4000
-  const TEMPERATURE = 0.5 // 0:make nothing up | 2:make anything up
-
   try {
-    const endpoint = 'https://api.openai.com/v1/chat/completions';
-    const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.OPENAI_APIKEY}`
-    };
-
-    let prompt = transcribedText
-
-    // Set up the request data
     const data = {
       model: MODEL,
       max_tokens: MAXTOKENS,
       temperature: TEMPERATURE,
       messages: [
         { role: 'system', content: 'You are a helpful assistant.' },
-        { role: 'user', content: prompt }
+        { role: 'user', content: transcribedText }
       ]
     };
 
-    // Send the API request using axios
-    const response = await axios.post(endpoint, data, { headers });
+    const response = await axios.post(endpoint, data, { headers: chatGptHeaders });
     const chatGptResponse = response.data.choices[0].message.content;
     console.log('ChatGPT Response:', chatGptResponse);
 
-    // feed response to twilio
-    sendToElevenLabs(chatGptResponse, currentCallSid)
+    sendToElevenLabs(chatGptResponse, currentCallSid);
   } catch (error) {
     console.error('Error in sendToChatGPT:', error);
   }
 }
 
+// Function to send audio data to Twilio
+function sendAudioToTwilio(audioData) {
+  const base64AudioData = Buffer.from(audioData).toString('base64');
+
+  const message = {
+    event: 'media',
+    streamSid: streamId,
+    media: {
+      payload: base64AudioData,
+    },
+  };
+
+  ws.send(JSON.stringify(message));
+}
 
 // Function to send the ChatGPT response to ElevenLabs
 async function sendToElevenLabs(responseFromChatGPT, currentCallSid) {
   try {
-    const XI_API_KEY = process.env.ELEVENLABS_APIKEY;
-    const TTS_OUTPUT_PATH = "./output.wav";
-
-    const ttsUrl = "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM/stream"; // Make sure to set `voice-id`
-
-    const ttsHeaders = {
-      "Accept": "audio/mpeg",
-      "Content-Type": "application/json",
-      "xi-api-key": XI_API_KEY
-    };
-
     const ttsData = {
       "text": responseFromChatGPT,
       "model_id": "eleven_monolingual_v1",
@@ -92,39 +105,28 @@ async function sendToElevenLabs(responseFromChatGPT, currentCallSid) {
       }
     };
 
-    const ttsResponse = await axios.post(ttsUrl, ttsData, {
-      headers: ttsHeaders,
-      responseType: 'stream'
+
+
+    const ttsResponse = await axios.post(ttsUrl, ttsData, { headers: ttsHeaders, responseType: 'stream' });
+
+    
+
+    // Handle data events from the ElevenLabs stream and send it to Twilio
+    ttsResponse.data.on('data', (chunk) => {
+      sendAudioToTwilio(chunk);
     });
 
-    const outputStream = fs.createWriteStream(TTS_OUTPUT_PATH);
-    ttsResponse.data.pipe(outputStream);
-
-    return new Promise((resolve, reject) => {
-      outputStream.on('finish', async () => {
-        console.log('ElevenLabs TTS output written successfully.');
-
-        // Check if the output.wav file exists
-        if (fs.existsSync(TTS_OUTPUT_PATH)) {
-          try {
-            await sendRawAudio(currentCallSid, websocketStreamUrl); // Use websocketStreamUrl here
-            console.log('Finished sending audio over WebSocket');
-            resolve();
-          } catch (error) {
-            reject(error);
-          }
-        } else {
-          console.log('output.wav file does not exist.');
-          resolve();
-        }
-      });
-
-      outputStream.on('error', error => {
-        console.error('Error writing TTS output:', error);
-        reject(error);
-      });
+    // Handle the end of the ElevenLabs stream
+    ttsResponse.data.on('end', () => {
+      console.log('ElevenLabs TTS stream ended.');
+      // You can perform any cleanup or additional actions here.
     });
 
+    // Handle errors from the ElevenLabs stream
+    ttsResponse.data.on('error', (error) => {
+      console.error('Error in ElevenLabs TTS stream:', error);
+      // You can handle errors as needed.
+    });
   } catch (error) {
     console.error('Error in sendToElevenLabs:', error.message);
   }
@@ -133,77 +135,20 @@ async function sendToElevenLabs(responseFromChatGPT, currentCallSid) {
 async function initBotResponse() {
   // Terminate AssemblyAI session
   assembly.send(JSON.stringify({ terminate_session: true }));
-
   // Send the transcribed data to ChatGPT
   await sendToChatGPT(transcribedText);
-
-  console.log('init bot complete');
+  console.log('Init bot complete');
 }
 
-async function sendRawAudio(callSid, streamUrl) {
-  console.log('send raw audio');
-  console.log(callSid);
-  console.log(streamUrl);
-
-  try {
-    // Load the output.wav file from your root directory
-    const audioFilePath = './output.wav'; // Update with the correct path if needed
-    const audioData = fs.readFileSync(audioFilePath);
-
-    // streaming data coming in frm ELEVENLABS 
-    // convert that to base 64 and add to payload
-    
-    // Base64 encode the audio data
-    const base64AudioData = Buffer.from(audioData).toString("base64");
-
-    // Create a message object with the "media" event and audio payload
-    const message = {
-      event: "media",
-      streamSid: streamId, // Replace with your streamSid
-      media: {
-        payload: base64AudioData
-      }
-    };
-    console.log(message);
-
-    // Send the message as a JSON string to the WebSocket client
-    ws.send(JSON.stringify(message));
-
-    console.log('Finished sending audio to Twilio');
-  } catch (error) {
-    console.error('Error in sendRawAudio:', error);
-  }
-}
-async function sendAudioOverWebSocket(streamUrl) {
-  // Check if the WebSocket connection is established
-  if (!ws) {
-    console.error("WebSocket connection not established.");
-    return;
-  }
-
-  // Send the audio chunks over the WebSocket connection
-  chunks.forEach(chunk => {
-    ws.send(chunk);
-  });
-
-  console.log('Finished sending audio over WebSocket');
-}
-
-
-// Handle Web Socket Connection
 wss.on("connection", function connection(connection) {
   console.log("New Connection Initiated");
-  // Assign the WebSocket instance to the global variable
   ws = connection;
-  // console.log(client);
 
   ws.on("message", function incoming(message) {
     if (!assembly)
       return console.error("AssemblyAI's WebSocket must be initialized.");
 
-    // Parse the incoming message as JSON
     const msg = JSON.parse(message);
-    // console.log(msg.media.payload);
     const text = msg.text;
     
     if (msg.message_type === "PartialTranscript") {
@@ -215,7 +160,6 @@ wss.on("connection", function connection(connection) {
     switch (msg.event) {
       case "connected":
         console.log(`A new call has connected.`);
-
         // Handle AssemblyAI's messages and errors
         assembly.onerror = console.error;
 
@@ -253,15 +197,12 @@ wss.on("connection", function connection(connection) {
           });
         };
 
-        // if there has been 3s of time, send the message
-        // Log the transcribed text
         setTimeout(function() {initBotResponse()}, 5000);
-
         break;
       case "start":
         console.log(`Starting Media Stream ${msg.streamSid}`);
-        streamId = msg.streamSid
-        currentCallSid  = msg.start.callSid
+        streamId = msg.streamSid;
+        currentCallSid  = msg.start.callSid;
         break;
       case "media":
         // Extract raw audio data from Twilio's media payload
@@ -291,26 +232,20 @@ wss.on("connection", function connection(connection) {
           }
         });
 
-        if (chunks.length >= 5) {
+        if (chunks.length >= TWILIO_AUDIO_CHUNK_THRESHOLD) {
           const audioBuffer = Buffer.concat(chunks);
           const encodedAudio = audioBuffer.toString("base64");
           assembly.send(JSON.stringify({ audio_data: encodedAudio }));
-          
           chunks = [];
         }
-
         break;
       case "stop":
         console.log(`Call Has Ended`);
-
-        // Terminate AssemblyAI session
         assembly.send(JSON.stringify({ terminate_session: true }));
-
         break;
     }        
   });
 });
-
 // Handle GET requests - this will likely be the login
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
